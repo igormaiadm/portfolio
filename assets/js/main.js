ced141ab174f57c2509322a336a8df62d3cace91
Igor <igormaiadm@gmail.com>
2024-06-18 17:36:15 -0300

    Skills animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -161,7 +161,7 @@
   if (skillsContent) {
     new Waypoint({
       element: skillsContent,
-      offset: '200%',
+      offset: '300%',
       handler: function(direction) {
         let progress = select('.progress .progress-bar', true);
         progress.forEach((el) => {
